3110137c855d721b6343cc2677fc3f1cfe6c9c7b
ibrahim yeryaran <ibrahimyeryaran93@gmail.com>
2025-05-08 01:16:49 +0300

    fix: update base URL for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 import { TanStackRouterVite } from '@tanstack/router-plugin/vite';
 
 export default defineConfig({
-  base: '/fun-chat-main/',
+  base: '/fun-chat/',
 
   build: {
     minify: true,
